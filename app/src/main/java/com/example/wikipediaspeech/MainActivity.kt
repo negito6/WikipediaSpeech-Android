ed241a66db73482a039646e128bcfef275e02098
Toru YAGI <negito6@users.noreply.github.com>
2019-10-05 12:52:40 +0900

    Fix views

--- app/src/main/java/com/example/wikipediaspeech/MainActivity.kt
+++ app/src/main/java/com/example/wikipediaspeech/MainActivity.kt
@@ -15,17 +15,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
+        setClickLisnter()
+    }
+
+    private fun setClickLisnter() {
+        plus.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
+        return false
     }
+
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle action bar item clicks here. The action bar will
