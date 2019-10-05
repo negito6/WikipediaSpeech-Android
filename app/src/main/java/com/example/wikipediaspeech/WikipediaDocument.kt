5caaaa7cb67efc21b5b1ec4970484b9f67c50a50
Toru YAGI <negito6@users.noreply.github.com>
2019-10-05 20:32:01 +0900

    dom removed

--- app/src/main/java/com/example/wikipediaspeech/WikipediaDocument.kt
+++ app/src/main/java/com/example/wikipediaspeech/WikipediaDocument.kt
@@ -2,14 +2,36 @@ package com.example.wikipediaspeech
 
 import android.content.Context
 import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
 
 class WikipediaDocument(val context: Context, val document: Document) {
     fun body(): String {
-        return document.getElementById(context.getString(R.string.wikipedia_dom_id_body)).text()
+        return bodyDom().text()
     }
 
-    fun bodyBlocks(): List<String> {
-        return body().replace(context.getString(R.string.wikipedia_body_text_edit), "\n").split("\n")
+    fun bodyDom(): Element {
+        return document.getElementById(context.getString(R.string.wikipedia_dom_id_body))
+    }
+
+    fun readableBody(): String {
+        val dom = bodyDom()
+        arrayOf("table", "dl").forEach { name ->
+            dom.getElementsByTag(name).forEach { elem ->
+                elem.parent().insertChildren(elem.siblingIndex(), elem.childNodes())
+                elem.remove()
+            }
+        }
+        arrayOf("mw-editsection", "reference").forEach { name ->
+            dom.getElementsByClass(name).forEach { elem ->
+                elem.parent().insertChildren(elem.siblingIndex(), elem.childNodes())
+                elem.remove()
+            }
+        }
+        return dom.text()
+    }
+
+    fun contents(): List<String> {
+        return readableBody().split("\n")
     }
 
     fun title(): String {
@@ -17,6 +39,6 @@ class WikipediaDocument(val context: Context, val document: Document) {
     }
 
     fun speechTexts(): List<String> {
-        return listOf(title()) + bodyBlocks()
+        return listOf(title()) + contents()
     }
 }
