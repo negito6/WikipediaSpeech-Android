2315d838f509cbce29b1eaa00440069760625b5f
Toru YAGI <negito6@users.noreply.github.com>
2019-10-06 18:09:52 +0900

    removing element fixed

--- app/src/main/java/com/example/wikipediaspeech/WikipediaDocument.kt
+++ app/src/main/java/com/example/wikipediaspeech/WikipediaDocument.kt
@@ -9,7 +9,8 @@ class WikipediaDocument(val document: Document) {
         private const val TITLE_DELIMITER = " - "
         private const val BODY_DOM_ID = "mw-content-text"
         private val BODY_DOM_UNREADABLE_CLASSES = listOf<String>("mw-editsection", "reference", "toc", "reflist")
-        private val BODY_DOM_UNREADABLE_TAGS = listOf<String>("table", "dl")
+        private val BODY_DOM_UNREADABLE_TAGS = listOf<String>("table")
+        private val BODY_DOM_UNREADABLE_IDS = listOf<String>("infoboxCountry")
     }
 
     fun bodyDom(): Element {
@@ -27,6 +28,10 @@ class WikipediaDocument(val document: Document) {
             dom.getElementsByClass(name).forEach { elem ->
                 elem.remove()
             }
+        }
+
+        BODY_DOM_UNREADABLE_IDS.forEach { name ->
+            dom.getElementById(name).remove()
         }
         return dom.text()
     }
